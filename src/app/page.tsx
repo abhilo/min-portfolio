53f457759e91b21668f15c2a1e2af97bf1fcbc70
Abhi <sabhifirst@gmail.com>
2025-05-11 01:58:43 -0400

    updated icon cloud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,41 @@ import { DATA } from "@/data/resume";
 import Link from "next/link";
 import Markdown from "react-markdown";
 
+import IconCloud from "@/components/icon-cloud"
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
+
 const BLUR_FADE_DELAY = 0.04;
 
 export default function Page() {
@@ -103,6 +138,17 @@ export default function Page() {
             <h2 className="text-xl font-bold">Skills</h2>
           </BlurFade>
           <div className="flex flex-wrap gap-1">
+          <IconCloud iconSlugs={slugs} />
+          </div>
+
+        </div>
+      </section>
+      {/* <section id="skills">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 9}>
+            <h2 className="text-xl font-bold">Skills</h2>
+          </BlurFade>
+          <div className="flex flex-wrap gap-1">
             {DATA.skills.map((skill, id) => (
               <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
                 <Badge key={skill}>{skill}</Badge>
@@ -110,7 +156,7 @@ export default function Page() {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
       <section id="projects">
         <div className="space-y-12 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 11}>
@@ -188,7 +234,7 @@ export default function Page() {
                     dates={project.dates}
                     image={project.image}
                     links={project.links}
-                  />
+                  />t
                 </BlurFade>
               ))}
             </ul>
